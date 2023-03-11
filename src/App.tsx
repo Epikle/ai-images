4d83d4e235ab6d2db964fb8d191e01f562cc0bef
Epikle <98042337+Epikle@users.noreply.github.com>
2023-03-11 22:52:45 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import styles from './App.module.scss';
+
+const App = () => {
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const getImages = async () => {
+      try {
+        const { data } = await axios.get(
+          'https://raw.githubusercontent.com/Epikle/ai-images/main/images_list.json'
+        );
+
+        setImages(data.images);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    };
+
+    getImages();
+  }, []);
+
+  return (
+    <div className={styles.app}>
+      <h1>AI Images</h1>
+      <div className={styles.images}>
+        {images.map((image) => (
+          <img key={image} src={image} alt="ai generated" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
